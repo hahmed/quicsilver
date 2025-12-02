cfc6242de92a2b85ad677ecdf228c86f7da4cd52
Haroon Ahmed <haroon.ahmed25@gmail.com>
2025-12-02 21:26:21 +0000

    Fixed commented out server tests

--- test/server_test.rb
+++ test/server_test.rb
@@ -47,47 +47,40 @@ class ServerTest < Minitest::Test
     refute server.running?
   end
 
-  # Skipped: These tests use mock connection_data that crashes the C extension
-  # The C extension expects real MSQUIC connection pointers, not Ruby arrays
-  # TODO: Either mock the C calls properly or test at integration level
-  #
-  # def test_handle_stream_with_receive_event
-  #   server = create_server(4433, app: ->(env) { [200, {}, ["OK"]] })
-  #   stream_id = 1
-  #   connection_handle = 12345
-  #   connection_data = [connection_handle, 67890]
-  #
-  #   # First establish connection
-  #   Quicsilver::Server.handle_stream(connection_data, 0, Quicsilver::Server::STREAM_EVENT_CONNECTION_ESTABLISHED, "")
-  #
-  #   # Now send data
-  #   Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "test data")
-  #
-  #   connection = server.connections[connection_handle]
-  #   assert_not_nil connection, "Connection should exist"
-  #   stream = connection.get_stream(stream_id)
-  #   assert_not_nil stream, "Stream should exist"
-  #   assert_equal "test data", stream.buffer
-  # end
-  #
-  # def test_handle_stream_accumulates_data
-  #   server = create_server(4433, app: ->(env) { [200, {}, ["OK"]] })
-  #   stream_id = 2
-  #   connection_handle = 12345
-  #   connection_data = [connection_handle, 67890]
-  #
-  #   # First establish connection
-  #   Quicsilver::Server.handle_stream(connection_data, 0, Quicsilver::Server::STREAM_EVENT_CONNECTION_ESTABLISHED, "")
-  #
-  #   # Send multiple chunks
-  #   Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk1")
-  #   Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk2")
-  #   Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk3")
-  #
-  #   connection = server.connections[connection_handle]
-  #   stream = connection.get_stream(stream_id)
-  #   assert_equal "chunk1chunk2chunk3", stream.buffer
-  # end
+  def test_handle_stream_with_receive_event
+    server = create_server(4433, app: ->(env) { [200, {}, ["OK"]] })
+    stream_id = 1
+    connection_handle = 12345
+    connection_data = [connection_handle, 67890]
+
+    # Manually create connection
+    server.connections[connection_handle] = Quicsilver::Connection.new(connection_handle, connection_data)
+
+    # Now test receive
+    Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "test data")
+
+    stream = server.connections[connection_handle].get_stream(stream_id)
+    assert_equal "test data", stream.buffer.string
+  end
+
+  def test_handle_stream_accumulates_data
+    server = create_server(4433, app: ->(env) { [200, {}, ["OK"]] })
+    stream_id = 1
+    connection_handle = 12345
+    connection_data = [connection_handle, 67890]
+
+    # Manually create connection
+    server.connections[connection_handle] = Quicsilver::Connection.new(connection_handle, connection_data)
+
+    # Send multiple chunks
+    Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk1")
+    Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk2")
+    Quicsilver::Server.handle_stream(connection_data, stream_id, Quicsilver::Server::STREAM_EVENT_RECEIVE, "chunk3")
+  
+    connection = server.connections[connection_handle]
+    stream = connection.get_stream(stream_id)
+    assert_equal "chunk1chunk2chunk3", stream.buffer.string
+  end
 
   private
 
